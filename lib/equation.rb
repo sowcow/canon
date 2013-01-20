88b8eaa9f768ac2e525c6caed658e8c13615ed54
Alexander K <xpyro@ya.ru>
2013-01-20 17:03:54 +0600

    rescue

--- lib/equation.rb
+++ lib/equation.rb
@@ -6,10 +6,10 @@ module LastEquation
   classes.each do |klass|
     refine klass do
       def == other
-        super.tap { |result| $last_eq = "#{self} == #{other}" } 
+        super.tap { |result| $last_eq = "#{self.inspect} == #{other.inspect}" } 
       end
       def === other
-        super.tap { |result| $last_eq = "#{self} === #{other}" } 
+        super.tap { |result| $last_eq = "#{self.inspect} === #{other.inspect}" } 
       end  
     end
   end
@@ -19,12 +19,15 @@ class Equation < Struct.new :equation
   using LastEquation
 
   def eval object, equation=equation
-    object.instance_eval equation
+    object.instance_eval equation rescue :error
   end
   def text object
     eval(object)
     $last_eq
   end
+  def result object
+    [eval(object), $last_eq].tap { |x| x[1] = equation if x[0] == :error }
+  end  
 end
 
 def Eq block
@@ -44,6 +47,10 @@ if __FILE__ == $0
   raise unless Eq('(1..3) === a').eval(stub(a: 4)) == false
   raise unless Eq('(1..3) === a').text(stub(a: 3)) == '1..3 === 3'
   raise unless Eq('(1..3) === a').text(stub(a: 4)) == '1..3 === 4'
+  
+  raise unless Eq('(1..3) === a').result(stub(a: 4)) == [false, '1..3 === 4']
+
+  raise unless Eq('a == 2').result(12345) == [:error, 'a == 2']
 
   puts 'OK'
 end
