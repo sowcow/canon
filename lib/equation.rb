bb545b716e2335f6194c9ed1f5168685e4121c1a
Alexander K <xpyro@ya.ru>
2013-01-20 15:36:18 +0600

    test

--- lib/equation.rb
+++ lib/equation.rb
@@ -33,14 +33,17 @@ end
 
 
 
-raise unless Eq('a == 2').eval(stub(a: 2)) == true
-raise unless Eq('a == 2').eval(stub(a: 3)) == false
-raise unless Eq('a == 2').text(stub(a: 2)) == '2 == 2'
-raise unless Eq('a == 2').text(stub(a: 3)) == '3 == 2'
-raise unless Eq('(1..3) === a').eval(stub(a: 3)) == true
-raise unless Eq('(1..3) === a').eval(stub(a: 4)) == false
-raise unless Eq('(1..3) === a').text(stub(a: 3)) == '1..3 === 3'
-raise unless Eq('(1..3) === a').text(stub(a: 4)) == '1..3 === 4'
+if __FILE__ == $0
+  require 'ostruct'; def stub param; OpenStruct.new param end
 
-puts 'OK'
-BEGIN{ require 'ostruct'; def stub param; OpenStruct.new param end }
+  raise unless Eq('a == 2').eval(stub(a: 2)) == true
+  raise unless Eq('a == 2').eval(stub(a: 3)) == false
+  raise unless Eq('a == 2').text(stub(a: 2)) == '2 == 2'
+  raise unless Eq('a == 2').text(stub(a: 3)) == '3 == 2'
+  raise unless Eq('(1..3) === a').eval(stub(a: 3)) == true
+  raise unless Eq('(1..3) === a').eval(stub(a: 4)) == false
+  raise unless Eq('(1..3) === a').text(stub(a: 3)) == '1..3 === 3'
+  raise unless Eq('(1..3) === a').text(stub(a: 4)) == '1..3 === 4'
+
+  puts 'OK'
+end
