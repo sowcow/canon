855017cc4dd2597f947d6be0c45e52904ca05bf4
Alexander K <xpyro@ya.ru>
2013-01-21 19:45:07 +0600

    [4 classes]

--- extractor.rb
+++ extractor.rb
@@ -3,14 +3,53 @@
 #    - human readable validation code (kinda rspec)
 #    - computer readable result of validation
 # 2) extractor itself
+#
+# fuck memoization and other opt23n for now
+# fuck Struct with its to_a
+#
+# modify selectors if classes ar stupid like a1 a2 a3...?
+
+require 'my-sugar'
+require_delegation
 require 'nokogiri'
 Dir['lib/*.rb'].each { |x| require_relative x }
 
-page1 = Nokogiri::HTML '<html id=wow lang=en><div class=any>any text</html>'
-page2 = Nokogiri::HTML '<html id=wow lang=ru><div class=any>other text</html>'
+require 'pp'
+require 'pry'
+load 'lib/test_helper'
+
+
+
+class NodeInfo
+  def initialize node, interest; @node = node
+    @info = {Selector[node].to_s => Scan[node, interest].hash}
+  end
+  def self.[] *a; new *a end
+
+  def hash; @info end
+  def key; @info.keys[0] end
+  def value; @info.values[0] end
+
+  private
+  attr_reader :node
+end
+
+
+class Scan
+  def initialize node, measurements; @node = node
+                                           # same names => loss of values
+    @scan = Hash[ *[*measurements].map { |x| [x.name, x.scan(node)] }.flatten(1)]
+  end
+  def self.[] *a; new *a end
+
+  def hash; @scan end
+
+  private
+  attr_reader :node
+end
+
 
 class Selector
-  attr_reader :node # for local usage
   def initialize node; @node = node
     @selector = parents.to_a + [this]
   end
@@ -20,6 +59,7 @@ class Selector
   def to_s; @selector * ' > ' end
 
   private
+  attr_reader :node
   def parents
     node.respond_to?(:parent) ? Selector[node.parent] : []
   end
@@ -29,38 +69,186 @@ class Selector
   def inspect; to_s.inspect end
 end
 
-def selector node
-  path = (selector(node.parent) rescue []).flatten
-  this = [[node.name, node[:id]].compact * '#', node[:class]].compact * '.'
-    # ||node[:class]].compact * '-'
-  path + [this]
+
+class Measurement
+  def initialize given
+    case given
+    when String
+      @name = given
+      @proc = ->(node){ node.instance_eval(given) } 
+    when Hash
+      raise 'o0' if given.keys.count != 1
+      @name = given.keys[0]
+      @proc = given.values[0]
+    when Array
+      raise 'o0' if given.count != 2
+      @name = given[0]
+      @proc = given[1]      
+    else raise :unknown! end
+  end
+  def self.[] *a; new *a end  
+
+  def name; @name end
+  def scan node; @proc[node] end
 end
 
-nodes = []
-page1.traverse do |node|
-  nodes << {selector: Selector[node], node: node}
+class Extractor #; is Model(:interest, ) # *init_params, {procs / state}
+  attr_reader :state
+  def initialize interest; @interest = interest; @state = {} end
+  def self.[] *a; new *a end
+  
+  def feed page
+    html(page).traverse do |node|
+      info = NodeInfo[node, interest]
+      if @state[info.key]
+        @state[info.key] = smart_merge(@state[info.key], info.value)
+      else
+        merge! info
+      end
+    end
+  end
+
+  private
+  attr_reader :interest
+  def html *a; Nokogiri::HTML *a end    
+  def merge! info
+    @state.merge! info.hash
+  end
+  def smart_merge scan1, scan2
+    scan1.keys.each_with_object({}) do |key,result|
+      result[key] = scan1[key] if scan1[key] == scan2[key]
+    end
+    # raise :todo
+  end  
 end
-# puts nodes
+
+# attributes.map{|x,y|[x,y.value]}]rescue{}
+
+# measurements = %w"to_s text children.count".map { |x| Measurement[x] }
+# measurements = %w"text".map { |x| Measurement[x] }
+
+# page1 = '<html id=wow lang=en><div class=any>any text</html>'
+# page2 = '<html id=wow lang=ru><div class=any>other text</html>'
+
+# model = Extractor.new measurements
+# model.feed page1
+# pp model.state
+# p '='*10
+# model.feed page2
+# pp model.state
 
 
+if __FILE__ == $0
 
+  # stubs...
+  node = stub(name: 'hey', value: 123)
+  def node.[] any; nil end
+  sub_node = node.clone
+  def sub_node.name; 'sub' end
+  $node = node; def sub_node.parent; $node end
+  interest = [Measurement['name'],Measurement['value']]
+  # stubs...
+
+  raise unless %w"to_s text children.count".map { |x| Measurement[x] }.count == 3
+  raise unless %w"to_s text children.count".map { |x| Measurement[x] }.map(&:name) == %w"to_s text children.count"
+  raise unless Measurement['to_s'].scan(123) == '123'
+
+  raise unless Scan[node, Measurement['name']].hash.keys.count == 1
+  raise unless Scan[node, [Measurement['name'],Measurement['value']]].hash.keys.count == 2
+  raise unless Scan[node, [Measurement['name'],Measurement['value']]].hash.keys == ['name','value']
+  raise unless Scan[node, [Measurement['name'],Measurement['value']]].hash.values == ['hey',123]
+  
+  raise unless Selector[node].to_s == 'hey'
+  raise unless Selector[node].to_a == ['hey']
+  raise unless Selector[sub_node].to_a == ['hey','sub']
+  raise unless Selector[sub_node].to_s == 'hey > sub'
+
+  raise unless NodeInfo[node, interest].hash == {"hey"=>{"name"=>"hey", "value"=>123}}
+  raise unless NodeInfo[node, interest].key == "hey"
+  raise unless NodeInfo[node, interest].value == {"name"=>"hey", "value"=>123}
+
+  raise unless Extractor[interest].state == {}
+
+  e = Extractor[Measurement['self[:value]']]
+  e.feed '<div value=123>'
+  raise unless e.state["document > html > body > div"] == {"self[:value]"=>"123"}
+  e.feed '<div value=465>'
+  raise unless e.state["document > html > body > div"] == {}
+  puts 'OK'
+end
+__END__
+interest = %w[to_s text].map { |msg| ->(node){ Hash[msg, node.send(msg)] }} +
+           %w[node.children.count].map { |proc| ->(node){ Hash[proc, eval(proc)] }} +
+           [->(node){ Hash[node.attributes.map {|x,y|[x,y.value]}] rescue {} }]
+__END__
+
+
+def scan page; page = Nokogiri::HTML page
+  nodes = []
+  page.traverse do |node|
+    nodes << {selector: Selector[node], node: node}
+  end
+end
 
 measurements = %w[to_s text].map { |msg| ->(node){ Hash[msg, node.send(msg)] }} +
                %w[node.children.count].map { |proc| ->(node){ Hash[proc, eval(proc)] }} +
                [->(node){ Hash[node.attributes.map {|x,y|[x,y.value]}] rescue {} }]
 
-nodes.each do |element|; node = element[:node]
-  element[:measurements] = measurements.each_with_object({}){ |get,o| o.merge! get[node] }
+
+# nodes = scan page1
+# puts nodes
+
+def process! nodes, measurements
+  nodes.each do |element|; node = element[:node]
+    element[:measurements] = measurements.each_with_object({}){ |get,o| o.merge! get[node] }
+    # element.delete :node
+    element[:node] = nil
+  end
+  nodes
+end; alias process process!
+
+nodes = process(scan(page1), measurements)
+
+
+class Node
+  attr_reader :data
+  def initialize node; @node = node
+    @data = data_for @node
+  end
+
+  private # helpers
+  def data_for node
+    node
+  end
 end
 
-# puts
-puts nodes
+
+class PageModel 
+  attr_reader :nodes
+  def initialize; @nodes = {} end
+
+  def scan page
+    HTML(page).traverse do |node|
+      scanned = Scan.new node
+      if @nodes[scanned.selector]
+        
+      else
+        @nodes[scanned.selector] = scanned
+      end
+    end
+  end
+
+  delegate :HTML, to: Nokogiri
+end
 
 
-exit 0
 
 
+require 'yaml'
+puts YAML.dump nodes
 
+
+__END__
 # remove scripts?
 def process node
   stuff = %w[name text to_s]
